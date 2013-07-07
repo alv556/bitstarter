b9d838a2a1e968c479ff29765b3fa1de044e5d75
Ales Vit <alv556@gmail.com>
2013-07-07 23:12:23 +0200

    Corrected web.js

--- web.js
+++ web.js
@@ -2,11 +2,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var infile = "index.html";
-var buffer = new Buffer(100);
 
 app.get('/', function(request, response) {
-  buffer.write(fs.readFileSync(infile), 'utf-8'); 
-  response.send(buffer.toString("utf8") );
+  var buffer = new Buffer(fs.readFileSync(infile), "utf-8")
+  response.send(buffer.toString("utf-8") );
 });
 
 var port = process.env.PORT || 5000;
