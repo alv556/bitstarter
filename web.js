24bb3219e48be2c297b2a51ec063dedc947f4bb1
Ales Vit <alv556@gmail.com>
2013-08-02 11:49:59 +0000

    web.js updated

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var app = express.createServer(express.logger());
 var fs = require('fs');
 var infile = "index.html";
 
+var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync(infile), "utf-8")
-  response.send(buffer.toString("utf-8") );
+  var html = fs.readFileSync(infile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
